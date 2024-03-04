30489f9390e213c435ed6c598a4fbba2f010ebde
Shakhzod Toyirov <shahzodtoyirov99@gmail.com>
2024-03-04 11:03:50 +0500

    add eslint hucky and pretter

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,13 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
 
 // If you want your app to work offline and load faster, you can change
